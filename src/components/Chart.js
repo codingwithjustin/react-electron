70f6d00352deb52f01d692dd3b3a7c496e6f29c6
pxltr <tymon.zaniewski.pl@gmail.com>
2021-06-11 20:56:05 +0200

    .

---
+++ src/components/Chart.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import {Line} from 'react-chartjs-2'
+
+class Chart extends Component{
+
+	render() {
+		return (
+			<div className="chart">
+
+			</div>
+		)
+	}
+}
+
+export default Chart
---
+++ src/components/Info.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class Info extends Component{
+
+	render() {
+		return (
+			<div className="info">
+				
+			</div>
+		)
+	}
+}
+
+export default Info
